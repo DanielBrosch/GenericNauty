c842fcd628a095eda4a6f28cae058af4d7144fa3
Documenter.jl <documenter@juliadocs.github.io>
2024-08-07 13:27:34 +0000

    build based on 22c35fe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GenericNauty","category":"page"},{"location":"#GenericNauty","page":"Home","title":"GenericNauty","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GenericNauty.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GenericNauty]","category":"page"}]
+}
