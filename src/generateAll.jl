37a89ce06fad717f6be7b0d819a8a5cfd2900f08
Daniel Brosch <73886037+DanielBrosch@users.noreply.github.com>
2024-08-12 14:40:56 +0200

    formatting

--- src/generateAll.jl
+++ src/generateAll.jl
@@ -6,11 +6,10 @@ Generates a list of all models of type `T` up to isomorphism on `maxVertices` ve
 """
 function generateAll(::Type{T}, maxVertices::Int) where {T<:Model}
     models::Vector{T} = T[T()]
-    for i = 1:maxVertices
+    for i in 1:maxVertices
         @info "Extending to $i vertices"
         extended::Vector{T} = reduce(vcat, extend(G) for G in models)
         models = unique(canonical.(extended))
     end
     return models
 end
-
